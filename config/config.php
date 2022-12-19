2ce1086b27d15f5ef031f38c0b739c1530fca50a
Haris Ratno Pambudi <harisratnopambudi@gmail.com>
2022-12-19 15:15:38 +0700

    first commit

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<haris','mikhmon>|>aWOBo6WhmpakamFjcQ==');
+$data['AshrafHotspot'] = array ('1'=>'AshrafHotspot!192.168.10.1','AshrafHotspot@|@haris','AshrafHotspot#|#aWOBo6WhmpakamFjcQ==','AshrafHotspot%AshrafHotspot','AshrafHotspot^ashrafhotspot.net','AshrafHotspot&Rp ','AshrafHotspot*0877 8447 7751','AshrafHotspot(','AshrafHotspot)','AshrafHotspot=30','AshrafHotspot@!@enable','AshrafHotspot#!#');
